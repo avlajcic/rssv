1ff6b3485801514ad648990dfe3b0902d24293ca
Ivana <i.skoric09@gmail.com>
2017-05-17 13:06:25 +0200

    file add

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 	
 	$file = 'data.txt';
-	if(isset(GET["alarm"])){
+	if(isset($_GET["alarm"])){
 		
 		$txt = $_GET['alarm']."\n";
 		file_put_contents($file, $txt, FILE_APPEND);
 	}
 		
-	
+	$myfile = fopen($file, "r") or die("Unable to open file!");
+	echo fread($myfile,filesize($file));
+	fclose($myfile);
 	
 	echo "aaaa";
 ?>
