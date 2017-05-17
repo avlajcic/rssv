2aa69b6733604c9625491eb018e9ae567d018d2b
Ivana <i.skoric09@gmail.com>
2017-05-17 12:46:06 +0200

    file add

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-	$file = 'data.txt';
+	//$file = 'data.txt';
 	if(isset($_POST["alarm"])){
 		
 		$txt = "$_POST["alarm"]\n";
 		file_put_contents($file, $txt, FILE_APPEND);
 	}
 		
-	$fileContent = file_get_contents($file, FILE_USE_INCLUDE_PATH);
+	//$fileContent = file_get_contents($file, FILE_USE_INCLUDE_PATH);
 
-	echo $fileContent;
-
+	//echo $fileContent;
+	echo "aaa";
 ?>
